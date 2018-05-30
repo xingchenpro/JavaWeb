ac60adf78b8f30b697a448c4a61c6baabef45bd2
黄良运 <1136513099@qq.com>
2018-05-30 21:34:29 +0800

    1

---
+++ Java/src/main/java/com/hly/java/mySynchronized/Thread1.java
@@ -0,0 +1,16 @@
+package com.hly.java.mySynchronized;
+
+/**
+ * @author :hly
+ * @date :2018/5/30
+ */
+public class Thread1 implements Runnable {
+    @Override
+    public void run() {
+        synchronized (this){
+            for(int i =0;i<3;i++){
+                System.out.println();
+            }
+        }
+    }
+}
